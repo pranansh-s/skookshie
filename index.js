447816ec86a28c4852edb38023b29eb09f69dd71
Pranansh Singh <prananshsingh@gmail.com>
2021-06-13 04:56:01 +0530

    deploy

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ const axios = require('axios');
 const youtubeAudio = require('ytdl-core');
 const querystring = require('querystring');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://skookshie.herokuapp.com]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 
 app.get('/youtube', async(req, res) => {
     let youtubeResults = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${querystring.escape(req.query.search)}&type=video&maxResults=1&videoCategoryId=10&key=${process.env.GOOGLE_API}`);
